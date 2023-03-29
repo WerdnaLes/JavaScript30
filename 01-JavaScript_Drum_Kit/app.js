8a8887b222674d90c7ddcf6d6da3d63dc1303804
werdnales <werdnales@gmail.com>
2023-03-29 22:19:31 +0300

    Make a func for btn listener

--- 01-JavaScript_Drum_Kit/app.js
+++ 01-JavaScript_Drum_Kit/app.js
@@ -1,4 +1,11 @@
-window.addEventListener("keydown", (element) => {
+window.addEventListener("keydown", playSound);
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => {
+  key.addEventListener("transitionend", removeTransition);
+});
+
+function playSound(element) {
   const audio = document.querySelector(`audio[data-key="${element.code}"]`);
   const key = document.querySelector(`div[data-key="${element.code}"]`);
   if (!audio) {
@@ -7,14 +14,9 @@ window.addEventListener("keydown", (element) => {
   audio.currentTime = 0; // rewind to the start
   audio.play();
   key.classList.add("playing");
-});
-
-const keys = document.querySelectorAll(".key");
-keys.forEach((key) => {
-  key.addEventListener("transitionend", removeTransition);
-});
+}
 
 function removeTransition(event) {
-  //   if (event.propertyName !== "transform") return; // skip if it's not a 'transform'
+  //   if (event.propertyName !== "transform") return; // actually causes a bug if you press a button for longer
   this.classList.remove("playing");
 }

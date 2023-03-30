bba78ecb944e051db59b9dd3ad9f63685a79396d
werdnales <werdnales@gmail.com>
2023-03-30 12:58:41 +0300

    Resolve a bug with hands when they reach 90deg

--- 02-JS_and_CSS-clock/app.js
+++ 02-JS_and_CSS-clock/app.js
@@ -1,6 +1,7 @@
 const hoursHand = document.querySelector(".hour-hand");
 const minutesHand = document.querySelector(".min-hand");
 const secondHand = document.querySelector(".second-hand");
+const hand = document.querySelector(".hand");
 
 setInterval(setDate, 1000);
 
@@ -12,25 +13,32 @@ function setDate() {
 }
 
 function rotateHours(date) {
-  const hours = date.getHours()
+  const hours = date.getHours();
   const hoursDegrees = (hours / 12) * 360 + 90;
-//   hoursHand.style.background='red'
-  hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
-
+  //   hoursHand.style.background='red'
+  hoursHand.style.transform = setRotate(hoursDegrees, hoursHand);
 }
 
 function rotateMinutes(date) {
   const minutes = date.getMinutes();
   const minutesDegrees = (minutes / 60) * 360 + 90;
-  minutesHand.style.background='pink'
-  minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  minutesHand.style.background = "pink";
+  minutesHand.style.transform = setRotate(minutesDegrees, minutesHand);
 }
 
 function rotateSeconds(date) {
   const seconds = date.getSeconds();
   const secondsDegrees = (seconds / 60) * 360 + 90;
-  secondHand.style.background='red'
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-  console.log(secondsDegrees);
+  secondHand.style.background = "red";
+  secondHand.style.transform = setRotate(secondsDegrees, secondHand);
 }
 
+// Resolve the problem of 'jumping' hands when they reach 90deg again:
+function setRotate(deg, pointer) {
+  if (deg === 90) {
+    pointer.style.transition = "all 0s";
+  } else {
+    pointer.style.transition = "all 0.05s cubic-bezier(0.1, 2.7, 0.58, 1)";
+  }
+  return `rotate(${deg}deg)`;
+}

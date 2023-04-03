cffa35d2945d50e793bab5a6875139a63bd4fbdb
werdnales <werdnales@gmail.com>
2023-04-03 14:36:22 +0300

    Finish 07 array excercises

--- 07-Array-Cardio-Day-2/app.js
+++ 07-Array-Cardio-Day-2/app.js
@@ -15,12 +15,32 @@ const comments = [
 
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19 or older?
-// Array.prototype.every() // is everyone 19 or older?
+const isAdult = people.some((person) => {
+  const currentYear = new Date().getFullYear();
+  return currentYear - person.year >= 19;
+});
 
+console.log({ isAdult });
+// Array.prototype.every() // is everyone 19 or older?
+const is19 = people.every((person) => {
+  const currentYear = new Date().getFullYear();
+  return currentYear - person.year >= 19;
+});
+
+console.log({ is19 });
 // Array.prototype.find()
 // Find is like filter, but instead returns just the one you are looking for
 // find the comment with the ID of 823423
+const theID = comments.find((comment) => comment.id === 823423);
+
+console.log(theID);
 
 // Array.prototype.findIndex()
 // Find the comment with this ID
 // delete the comment with the ID of 823423
+const index = comments.findIndex((comment) => comment.id === 823423);
+// comments.splice(index, 1);
+const newComments = [...comments.slice(0, index), ...comments.slice(index + 1)];
+
+console.table(comments);
+console.table(newComments);

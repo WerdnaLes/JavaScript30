46d16c97459e6816441d9034500e4c5a6b508b4a
werdnales <werdnales@gmail.com>
2023-04-15 10:56:03 +0300

    Add play and pause listeners, update progressBar

---
+++ 11-Custom-Video-Player/app.js
@@ -0,0 +1,41 @@
+// Get the elements:
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+
+// Build the functions:
+function togglePlay() {
+  video.paused ? video.play() : video.pause();
+  console.log(video.duration);
+
+  //      course expamle:
+  //   const method = video.paused ? "play" : "pause";
+  //   video[method]();
+}
+
+function updateButton() {
+  const icon = this.paused ? "►" : "❚ ❚";
+  toggle.textContent = icon;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function updateProgress() {
+  const percentage = (video.currentTime / video.duration) * 100;
+  console.log(percentage);
+  progressBar.style.flexBasis = `${percentage}%`;
+}
+
+// Hook up event listeners:
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", updateProgress);
+toggle.addEventListener("click", togglePlay);
+skipButtons.forEach((button) => button.addEventListener("click", skip));

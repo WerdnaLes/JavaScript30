694863dd0dc00941138fe5bfc88d2d82598d651d
werdnales <werdnales@gmail.com>
2023-04-19 14:43:01 +0300

    Finish day 13

--- 13-Slide-in-on-Scroll/app.js
+++ 13-Slide-in-on-Scroll/app.js
@@ -1,8 +1,35 @@
+// Hook document variables:
+
+const sliderImages = document.querySelectorAll(".slide-in");
+
+// Hook event listeners:
+
+window.addEventListener("scroll", debounce(checkSlide));
+
+// Functions:
+
+function checkSlide() {
+  sliderImages.forEach((sliderImage) => {
+    // half way through the image
+    const slideInAt =
+      window.scrollY + window.innerHeight - sliderImage.height / 2;
+    // bottom of the image
+    const imageBottom = sliderImage.offsetTop + sliderImage.height;
+    const isHalfShown = slideInAt > sliderImage.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      sliderImage.classList.add("active");
+    } else {
+      sliderImage.classList.remove("active");
+    }
+  });
+}
+
 function debounce(func, wait = 20, immediate = true) {
   let timeout;
   return () => {
-    let context = this,
-      args = arguments;
+    let context = this;
+    let args = arguments;
     let later = () => {
       timeout = null;
       if (!immediate) {

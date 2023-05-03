eb6e4bfb5f247bbec3cae94f3696b648f3e1c9e1
werdnales <werdnales@gmail.com>
2023-05-03 15:36:45 +0300

    Finished day 15 challenge

--- 15-Local-Storage/app.js
+++ 15-Local-Storage/app.js
@@ -1,5 +1,26 @@
+/*
+    //Challenge//
+    Make a:
+    * Delete All button;
+    * Check All button;
+    * Uncheck All button;
+    * Mirror it all to the localStorage;
+ */
+
 const addItems = document.querySelector(".add-items");
 const itemsList = document.querySelector(".plates");
+// challenge
+const checkerButton = document.querySelector("#checker-button");
+const deleteButton = document.querySelector("#deleteAll-button");
+let areAllChecked = localStorage.getItem("wasPressed") === "true";
+let checkerButtonText = localStorage.getItem("areAllChecked") || "Check All";
+
+checkerButton.addEventListener("click", checkAll);
+deleteButton.addEventListener("click", deleteMenu);
+
+checkerButton.value = checkerButtonText;
+
+// challenge
 const items = JSON.parse(localStorage.getItem("items")) || [];
 
 addItems.addEventListener("submit", addItem);
@@ -44,4 +65,33 @@ function toggleDone(event) {
 function updateLocalStorage() {
   localStorage.setItem("items", JSON.stringify(items));
   populateList(items, itemsList);
+}
+
+// challenge
+
+function updateCheckedStorage(areChecked, currentText) {
+  areChecked = String(areChecked);
+  localStorage.setItem("wasPressed", areChecked);
+  localStorage.setItem("areAllChecked", currentText);
+}
+
+function checkAll() {
+  if (items.length > 0) {
+    items.forEach((element) => {
+      element.done = !areAllChecked;
+    });
+    areAllChecked = !areAllChecked;
+    checkerButton.value = areAllChecked ? "Uncheck All" : "Check All";
+    updateCheckedStorage(areAllChecked, this.value);
+    updateLocalStorage();
+  }
+}
+
+function deleteMenu() {
+  localStorage.clear();
+  items.splice(0, items.length);
+  itemsList.innerHTML = "";
+  areAllChecked = false;
+  checkerButton.value = "Check All";
+  updateCheckedStorage(false, "Check All");
 }
